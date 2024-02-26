74932a6c9c2732093a281cd7784c76098fa52a70
Wendelsena <wendel4019@gmail.com>
2024-02-26 19:47:13 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -10,9 +10,10 @@ public class Reservation {
 	private Date checkIn;
 	private Date checkOut;
 
-	// Dando um formato padrão ao Date (Static para não precisar estanciar um DateFormat para cada Obj Reservation.
-	public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); 
-	
+	// Dando um formato padrão ao Date (Static para não precisar estanciar um
+	// DateFormat para cada Obj Reservation.
+	public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
 	public Reservation() {
 
 	}
@@ -38,29 +39,33 @@ public class Reservation {
 	public Date getCheckOut() {
 		return checkOut;
 	}
-	
+
 	public long duration() { // long é o int mais longo. Uso para calcular datas
 		long diff = checkOut.getTime() - checkIn.getTime(); // me devolve a diferença entre as datas em milisegundos.
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); // converte o valor diff em milesegundos para dia.
 	}
-	
-	//fazendo um update da data usando como argumento o checkIn e o checkOut
-	public void updateDates(Date checkIn, Date checkOut) {
+
+	// fazendo um update da data usando como argumento o checkIn e o checkOut
+	public String updateDates(Date checkIn, Date checkOut) {
+
+		// (se checkIn ou checkOut forem antes que now faça).
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update must be future dates";
+		}
+		if (!(checkOut.after(checkIn))) {
+			return "Check-out date must be after check-in date";
+		}
+
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null; // null para garantir que nenhum erro aconteca
 	}
-	
+
 	@Override // Sempre usamos no toString pq é um caso de sobreposição qualquer
 	public String toString() {
-		return "Room: "
-				+ roomNumber
-				+ ", check-in: "
-				+ sdf.format(checkIn)
-				+ ", check-out: "
-				+ sdf.format(checkOut)
-				+ ", "
-				+ duration()
-				+ " nights";
+		return "Room: " + roomNumber + ", check-in: " + sdf.format(checkIn) + ", check-out: " + sdf.format(checkOut)
+				+ ", " + duration() + " nights";
 	}
 
 }
